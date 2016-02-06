73434435a4fca9f3a6b0f62443344ef7ff099588
Sébastien Demanou <demsking@gmail.com>
2016-02-06 11:00:45 +0100

    Add the gulp file

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+﻿// Base Gulp File
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+// Task to Minify JS
+gulp.task('default', function() {
+    return gulp.src('./src/**/*.js')
+        .pipe(uglify())
+        .pipe(rename('windows.min.js'))
+        .pipe(gulp.dest('./dist'));
+});
